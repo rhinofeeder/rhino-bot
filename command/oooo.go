2f9def0ef7c1d84bb7e8fb709b0f1e2ae3a22247
thistle <cherryfizwiz@gmail.com>
2023-01-24 23:19:21 -0500

    update oooo command to replace non-o vowels and add tests

--- command/oooo.go
+++ command/oooo.go
@@ -1,21 +1,76 @@
 package command
 
+import (
+	"strings"
+)
+
+func isVowel(char byte) bool {
+	switch char {
+	case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+		return true
+	default:
+		return false
+	}
+}
+
 type OoooCommand struct{}
 
 func (oc *OoooCommand) Name() string {
 	return "oooo"
 }
 
+// Handle OOOO-ifying a provided string
+// - replace any "o"s if they exist
+// - if no "o"s exist, try to replace the vowel(s) closest to the center of the word
 func (oc *OoooCommand) Handle(message string) (string, error) {
 	oooo := ""
-	for _, ch := range message {
-		if ch == 'o' || ch == 'O' {
-			oooo += " OOOO "
+	if strings.Contains(strings.ToLower(message), "o") {
+		for _, ch := range message {
+			if ch == 'o' || ch == 'O' {
+				oooo += " OOOO "
+			} else {
+				oooo += string(ch)
+			}
+		}
+	} else {
+		messageLength := len(message)
+		midpoint := messageLength / 2
+		vowelIndex := -1
+		if isVowel(message[midpoint]) {
+			vowelIndex = midpoint
 		} else {
-			oooo += string(ch)
+			// Search outwards to find the vowel closest to the center
+			for i := 1; i < messageLength-midpoint; i++ {
+				if beforeIndex := midpoint - i; (beforeIndex) > 0 && isVowel(message[beforeIndex]) {
+					vowelIndex = beforeIndex
+					break
+				} else if afterIndex := midpoint + i; isVowel(message[afterIndex]) {
+					vowelIndex = afterIndex
+					break
+				}
+			}
+		}
+
+		if vowelIndex == -1 {
+			return "Can't find any vowels to replace MMMM", nil
+		}
+
+		// Check if adjacent letters are vowels
+		lowerIndex := vowelIndex
+		upperIndex := vowelIndex
+		if vowelIndex > 0 && isVowel(message[vowelIndex-1]) {
+			lowerIndex = vowelIndex - 1
+		} else if vowelIndex+1 < messageLength && isVowel(message[vowelIndex+1]) {
+			upperIndex = vowelIndex + 1
+		}
+
+		oooo = message[:lowerIndex] + " OOOO "
+		if upperIndex+1 < messageLength {
+			oooo += message[upperIndex+1:]
 		}
 	}
-	return oooo, nil
+
+	return strings.TrimSpace(oooo), nil
 }
 
 func (oc *OoooCommand) RequiresMod() bool {
