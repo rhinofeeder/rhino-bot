08bd543140812e4b85d8e4c20d9c8e9f89e27a68
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-03-10 11:57:13 -0500

    remove newline

--- behavior/conditional/dad.go
+++ behavior/conditional/dad.go
@@ -11,7 +11,6 @@ type DadConditional struct {
 
 func (dc *DadConditional) Handle(message string) (string, error) {
 	words := strings.Fields(message)
-
 	if len(words) > 1 && len(words) < 20 && strings.EqualFold(words[0], "I'm") && behavior.GenerateBool(50) {
 		return fmt.Sprintf("Hi %v, I'm rhinobot!", strings.Join(words[1:], " ")), nil
 	}
