8771d0bef9ffb6a39f55cce19afae0d15c6f9bcd
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-03-10 11:56:38 -0500

    change per PR feedback

--- behavior/conditional/dad.go
+++ behavior/conditional/dad.go
@@ -1,7 +1,6 @@
 package conditional
 
 import (
-	"errors"
 	"fmt"
 	"rhino-bot/behavior"
 	"strings"
@@ -12,11 +11,8 @@ type DadConditional struct {
 
 func (dc *DadConditional) Handle(message string) (string, error) {
 	words := strings.Fields(message)
-	if len(words) > 20 {
-		return "", errors.New("dad command limited to 20 words or fewer. tighten it up")
-	}
 
-	if len(words) > 1 && strings.EqualFold(words[0], "I'm") && behavior.GenerateBool(50) {
+	if len(words) > 1 && len(words) < 20 && strings.EqualFold(words[0], "I'm") && behavior.GenerateBool(50) {
 		return fmt.Sprintf("Hi %v, I'm rhinobot!", strings.Join(words[1:], " ")), nil
 	}
 	return "", nil
