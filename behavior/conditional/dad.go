8e9037e4c09b06f0c8d1514ac8d04c5a872d2738
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-03-09 22:28:29 -0500

    limit dad to 20 words as a start

--- behavior/conditional/dad.go
+++ behavior/conditional/dad.go
@@ -1,6 +1,7 @@
 package conditional
 
 import (
+	"errors"
 	"fmt"
 	"rhino-bot/behavior"
 	"strings"
@@ -11,6 +12,10 @@ type DadConditional struct {
 
 func (dc *DadConditional) Handle(message string) (string, error) {
 	words := strings.Fields(message)
+	if len(words) > 20 {
+		return "", errors.New("dad command limited to 20 words or fewer. tighten it up")
+	}
+
 	if len(words) > 1 && strings.EqualFold(words[0], "I'm") && behavior.GenerateBool(50) {
 		return fmt.Sprintf("Hi %v, I'm rhinobot!", strings.Join(words[1:], " ")), nil
 	}
